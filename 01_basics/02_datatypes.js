79f8952375c4148469b9d587ec902f18589e4c88
mahimasi <ms1906261@gmail.com>
2024-10-13 11:21:21 +0530

    datatypes  with examples

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs,not browser
+console.log(3
+    +
+    3
+) // code readability should be high 
+console.log("mahima singh");
+
+let name ="hitesh"  // string datatype
+let age = 20        // number  datatype
+let isLoggenIn = false // boolean  datatype
+
+// Type of Datatypes:
+// 1. primitive datatypes:
+// number =>  2 to power 53
+// bigint => for large number
+// string => '' or ""
+// boolean  => true/false
+// null => standalone value
+//undefined => unassigned value
+// symbol =>to find unique value
+
+
+ 
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null );     // object
+
+

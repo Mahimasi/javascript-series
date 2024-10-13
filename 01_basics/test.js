ca6b0c7dc54c3974f8cbae7e85aa36a84f3d852c
mahimasi <ms1906261@gmail.com>
2024-10-13 10:56:13 +0530

    first code

---
+++ 01_basics/test.js
@@ -0,0 +1,3 @@
+console.log("Hello Developers This is Mahi Singh a Web Developer ,How are you!");
+// TO run js file locally use command node filename.js
+// To run js file using online environment use command node path/ to/ folder

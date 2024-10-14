b9f2a2776c2108aa3fdbb8443cc8847b55f0c7f8
mahimasi <ms1906261@gmail.com>
2024-10-14 15:36:47 +0530

    discussion on operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,31 @@
+// Arithmetic operators examples:
+console.log(2 + 2);   // 4
+console.log(2 - 2);   // 0
+console.log(2 * 2);   //  4
+console.log(2 ** 3);  // 2 to the power of 3 (2^3 = 8)
+console.log(2 / 3);   //  0.66...
+console.log(3 % 2);   // Modulus: (3 % 2 = 1)
+console.log(2 % 3);   // Modulus: Remainder of 2 divided by 3 (2 % 3 = 2)
+
+//// String concatenation examples:
+//     let str1 = "hello";   // First string
+//     let str2 = "  mahiii";  // Second string with extra spaces
+
+//     let str3 = str1 + str2;  // Concatenates (joins) two strings
+//     console.log(str3);  //  "hello  mahiii"
+
+// //Mixing numbers and strings:
+//     console.log("1" + 2);    // String + number => (string) // 12
+//     console.log("1" + "2");  // String + string =>  (string) //12 
+//     console.log(1 + "2");    // Number + string: 1 is converted to string, result => (string) //12
+//     console.log("1" + 2 + 2);  // Left to right: First "1" + 2 = "12" (string), then "12" + 2 = "122"
+//     console.log(1 + 2 + "2");  // Left to right: First 1 + 2 = 3 (number), then 3 + "2" = "32" (string)
+
+
+console.log(true);  //true
+console.log(+true);  // 1
+console.log(+"");    // 0
+
+
+
+   

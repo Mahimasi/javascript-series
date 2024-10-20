b2a23f6ef2abfd4595687fcd86a4eaeb162a4bcb
mahimasi <ms1906261@gmail.com>
2024-10-20 11:08:03 +0530

    iife and types

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,40 @@
+// Immediately Invoked Function Expression (IIFE)
+//IIFE used to prevent/avoid  the pollution of globle scope
+// IIFE = ()()  ,one() is for fn def. and another() for calling fn
+
+// types of IIFE:
+// 1. named IIFE:
+
+// (function chai (){
+//     console.log(`DB CONNECTED`);  // DB CONNECTED
+//     })()
+
+// 2. Unnamed IIFE: using arrow fn
+
+// (() => {
+//   console.log(`DB CONNECTED TWO`); // DB CONNECTED TWO
+// })()
+
+// Passing parameters in unnamed IIFE:
+//  ( (username) => {
+//     console.log(`${username}, you are the best `);
+//      }
+//  )("Mahi")  // output: Mahi, you are the best
+
+
+  // Merging two IIFE FN  one is named and another is arrow fn 
+  // NOTE:while merging two fn, we have to add semicolon afte one fn
+  // 1. Named IIFE:
+
+  (function chai() {
+    console.log(`DB CONNECTED`); 
+  }
+)();
+
+// 2. Unnamed IIFE
+
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`); 
+})("mahima");
+//output : DB CONNECTED
+// DB CONNECTED TWO mahima
